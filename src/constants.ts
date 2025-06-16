0e2eb0b119e74eb58d3bc5bf05f80714a401cdf9
Ravi Pinnamaneni <rpinnama@gmail.com>
2025-06-16 00:29:48 -0500

    Update project-specific values

--- src/constants.ts
+++ src/constants.ts
@@ -19,25 +19,24 @@ interface Social {
 export const SOCIALS: Social[] = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/rpinnama",
     linkTitle: `${SITE.title} on Github`,
     icon: IconGitHub,
   },
   {
     name: "X",
-    href: "https://x.com/username",
+    href: "https://x.com/g3nbl0k",
     linkTitle: `${SITE.title} on X`,
     icon: IconBrandX,
-  },
-  {
+  },  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/username/",
+    href: "https://www.linkedin.com/in/ravi-pinnamaneni-2472a725/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
   },
   {
     name: "Mail",
-    href: "mailto:yourmail@gmail.com",
+    href: "mailto:rpinnama@gmail.com",
     linkTitle: `Send an email to ${SITE.title}`,
     icon: IconMail,
   },
